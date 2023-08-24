0095eb42265586ab129e05e1e9ce5a3e938f0cf9
ERIK-SECOGEM <erik.secogem@outlook.com>
2023-08-24 16:08:23 -0600

    Modificar layout menu: Modificar rutas con el helper route()

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -3,17 +3,17 @@
         <i class=" fas fa-building"></i><span>Home</span>
     </a>
     @can('ver-usuario')
-        <a class="nav-link" href="/ejemplo/public/usuarios">
+        <a class="nav-link" href="{{route('usuarios.index')}}">
             <i class=" fas fa-users"></i><span>Usuarios</span>
         </a>
     @endcan
     @can('ver-rol')
-        <a class="nav-link" href="/ejemplo/public/roles">
+        <a class="nav-link" href="{{route('roles.index')}}">
             <i class=" fas fa-user-lock"></i><span>Roles</span>
         </a>
     @endcan
     @can('ver-trabajador')
-        <a class="nav-link" href="/ejemplo/public/trabajadores">
+        <a class="nav-link" href="{{route('trabajadores.index')}}">
             <i class=" fas fa-laptop-house"></i><span>Trabajadores</span>
         </a>
     @endcan
