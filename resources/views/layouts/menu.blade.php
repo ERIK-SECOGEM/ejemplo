81edfcc924b27bec7ccbda68b6b1f2a4cf9713a0
ERIK-SECOGEM <erik.secogem@outlook.com>
2023-08-24 15:48:56 -0600

    Modificar layout menu: Proteger menú con la directiva @can()

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -2,13 +2,19 @@
     <a class="nav-link" href="/ejemplo/public/home">
         <i class=" fas fa-building"></i><span>Home</span>
     </a>
-    <a class="nav-link" href="/ejemplo/public/usuarios">
-        <i class=" fas fa-users"></i><span>Usuarios</span>
-    </a>
-    <a class="nav-link" href="/ejemplo/public/roles">
-        <i class=" fas fa-user-lock"></i><span>Roles</span>
-    </a>
-    <a class="nav-link" href="/ejemplo/public/trabajadores">
-        <i class=" fas fa-laptop-house"></i><span>Trabajadores</span>
-    </a>
+    @can('ver-usuario')
+        <a class="nav-link" href="/ejemplo/public/usuarios">
+            <i class=" fas fa-users"></i><span>Usuarios</span>
+        </a>
+    @endcan
+    @can('ver-rol')
+        <a class="nav-link" href="/ejemplo/public/roles">
+            <i class=" fas fa-user-lock"></i><span>Roles</span>
+        </a>
+    @endcan
+    @can('ver-trabajador')
+        <a class="nav-link" href="/ejemplo/public/trabajadores">
+            <i class=" fas fa-laptop-house"></i><span>Trabajadores</span>
+        </a>
+    @endcan
 </li>
